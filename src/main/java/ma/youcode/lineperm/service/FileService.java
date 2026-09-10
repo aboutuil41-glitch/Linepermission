c2828330ebd62d0560441b9d556f1677ba860ec6
aboutuil41-glitch <aboutuil41@gmail.com>
2026-09-10 12:30:05 +0100

    saveFile

--- src/main/java/ma/youcode/lineperm/service/FileService.java
+++ src/main/java/ma/youcode/lineperm/service/FileService.java
@@ -1,3 +1,13 @@
 package ma.youcode.lineperm.service;
 public class FileService {
+            public void saveFile(FileRecord files) {
+        try (FileWriter writer = new FileWriter("C:\\Users\\pc\\Documents\\java-bootcamp\\LinePermission\\src\\main\\resources\\files.txt", true)) {
+            writer.write(files.getName() + "," + files.getBelongsTo() + "," + files.getShare());
+            writer.write(System.lineSeparator());
+            System.out.println("Successfully written to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
 }
