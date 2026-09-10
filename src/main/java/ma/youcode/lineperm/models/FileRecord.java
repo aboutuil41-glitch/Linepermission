92f53db8b9919ffef834c3326c9d2528b5b3e417
aboutuil41-glitch <aboutuil41@gmail.com>
2026-09-10 12:29:12 +0100

    Create FileRecord.java

---
+++ src/main/java/ma/youcode/lineperm/models/FileRecord.java
@@ -0,0 +1,31 @@
+package ma.youcode.lineperm.models;
+
+import java.nio.file.Files;
+
+public class FileRecord {
+    String name;
+    String belongsTo;
+    String Share = "---";
+
+    public FileRecord setBelongsTo(String belongsTo) {
+        this.belongsTo = belongsTo;
+        return this;
+    }
+
+    public FileRecord setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBelongsTo() {
+        return belongsTo;
+    }
+
+    public String getShare() {
+        return Share;
+    }
+}
