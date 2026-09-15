51563229632bab6d2ddd2a99668263148179309a
aboutuil41-glitch <aboutuil41@gmail.com>
2026-09-15 14:05:53 +0100

    Added writing to access.log

--- src/main/java/ma/youcode/lineperm/service/LogsService.java
+++ src/main/java/ma/youcode/lineperm/service/LogsService.java
@@ -1,3 +1,22 @@
 package ma.youcode.lineperm.service;
 public class LogsService {
+    List<Logs> logs = new ArrayList<>();
+
+
+    private static final String LOG_FILE = "C:\\Users\\pc\\Documents\\java-bootcamp\\LinePermission\\src\\main\\resources\\access.log";
+
+    public void saveAllLogs() {
+        try (FileWriter writer = new FileWriter(LOG_FILE, false)) { // false = overwrite, not append (same style as FileService.saveAllFiles())
+            for (Logs l : logs) {
+                String time = l.getTime().format(DateTimeFormatter.ofPattern("HH:mm"));
+                writer.write(l.getDate() + ";" + time + ";" + l.getUser() + ";" + l.getAction() + ";" + l.getFile() + ".txt;" + l.getResult());
+                writer.write(System.lineSeparator());
+            }
+            System.out.println("Logs saved.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
+
 }
